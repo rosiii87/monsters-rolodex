9b38e8b3a599c8f111049572a00c14511e6fec31
Michal Rosa <michal.rosa@kiwi.com>
2020-01-05 18:39:11 +0100

    Updates

---
+++ precache-manifest.5f3f812a4ca75e0ea7ef22aba762e1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf26eb048239ded28837dc3feed1d48",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c599101f50fb274662cf",
+    "url": "/monsters-rolodex/static/css/main.7525e804.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c599101f50fb274662cf",
+    "url": "/monsters-rolodex/static/js/main.df9cb5f6.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
